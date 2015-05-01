b84edde340383fc6f94014fa9f3e18c21492a8db
vikene <vigneashsundar@live.com>
2015-05-01 21:02:23 +0530

    daemon init

---
+++ keep_daemon.c
@@ -0,0 +1,50 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<unistd.h>
+#include<syslog.h>
+#include<string.h>
+
+int main(void)
+{
+
+	pid_t pid, sid;
+	pid = fork();
+	if(pid<0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if(pid>0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	
+	umask(0);
+	
+	sid = setsid();
+	if(sid <0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	
+	if((chdir("/"))<0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+	
+	while(1)
+	{
+	
+	
+		sleep(30);
+	}
+	exit(EXIT_SUCCESS);
+	
+}
